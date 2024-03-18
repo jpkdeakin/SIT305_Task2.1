0062617b14815d65620f48217deeb0eefe8cda94
jpdyno <37175783+jpdyno@users.noreply.github.com>
2024-03-18 16:40:33 +1100

    Conversions working

--- app/src/main/java/com/example/week2_ui/MainActivity.java
+++ app/src/main/java/com/example/week2_ui/MainActivity.java
@@ -4,58 +4,138 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Spinner;
 import org.w3c.dom.Text;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     Button convertButton;
     EditText valueInput;
     TextView resultsOutput;
-
+    TextView resultUnit;
+    Spinner unitTypeSpinner;
     Spinner sourceUnitSpinner;
     Spinner destUnitSpinner;
 
-
-
-    String mystring;
+    ArrayAdapter<String> arrayLengthUnits;
+    ArrayAdapter<String> arrayWeightUnits;
+    ArrayAdapter<String> arrayTempUnits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Allocating widgets
         convertButton = findViewById(R.id.convert_button);
         valueInput = findViewById(R.id.value_input);
         resultsOutput = findViewById(R.id.results_output);
+        resultUnit = findViewById(R.id.result_unit);
         sourceUnitSpinner = findViewById(R.id.source_unit_spinner);
         destUnitSpinner = findViewById(R.id.destination_unit_spinner);
+        unitTypeSpinner = findViewById(R.id.unit_type_spinner);
+
+        // Allocating resources for unit arrays
+        ArrayAdapter<String> arrayLengthUnits = new ArrayAdapter<>(
+                this,android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.length_units));
+
+        ArrayAdapter<String> arrayWeightUnits = new ArrayAdapter<>(
+                this,android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.weight_units));
+
+        ArrayAdapter<String> arrayTempUnits = new ArrayAdapter<>(
+                this,android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.temp_units));
+
+
+        unitTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                switch(position) {
+                    case 0: // Length unit selected
+                        sourceUnitSpinner.setAdapter(arrayLengthUnits);
+                        destUnitSpinner.setAdapter(arrayLengthUnits);
+                        break;
+                    case 1: // Weight units selected
+                        sourceUnitSpinner.setAdapter(arrayWeightUnits);
+                        destUnitSpinner.setAdapter(arrayWeightUnits);
+                        break;
+                    case 2: // Temp unites selected
+                        sourceUnitSpinner.setAdapter(arrayTempUnits);
+                        destUnitSpinner.setAdapter(arrayTempUnits);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing.
+            }
+
+        });
 
         convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double valueOutputDouble;
+                Double valueOutputDouble = 0.0;
 
-                if (valueInput.getText().toString().length() != 0) {
-                    valueOutputDouble =
-                            lengthConversion(
-                                    sourceUnitSpinner.getSelectedItem().toString(),
-                                    destUnitSpinner.getSelectedItem().toString(),
-                                    Double.valueOf(valueInput.getText().toString())
-                            );
+                // Action determined by unit type selected
+                if (!valueInput.getText().toString().isEmpty()) {
 
-                    resultsOutput.setText(valueOutputDouble.toString());
+                    switch(unitTypeSpinner.getSelectedItemPosition()) {
+                        case 0: // Length unit selected
+                            valueOutputDouble =
+                                    lengthConversion(
+                                            sourceUnitSpinner.getSelectedItem().toString(),
+                                            destUnitSpinner.getSelectedItem().toString(),
+                                            Double.valueOf(valueInput.getText().toString())
+                                    );
+                            break;
+                        case 1: // Weight units selected
+                            valueOutputDouble =
+                                    weightConversion(
+                                            sourceUnitSpinner.getSelectedItem().toString(),
+                                            destUnitSpinner.getSelectedItem().toString(),
+                                            Double.valueOf(valueInput.getText().toString())
+                                    );
+                            break;
+                        case 2: // Temp unites selected
+                            valueOutputDouble =
+                                    tempConversion(
+                                            sourceUnitSpinner.getSelectedItem().toString(),
+                                            destUnitSpinner.getSelectedItem().toString(),
+                                            Double.valueOf(valueInput.getText().toString())
+                                    );
+                            break;
+                        default:
+                            break;
+                    }
+
+                    resultsOutput.setText(String.format(Locale.ENGLISH,"%.2f", valueOutputDouble));
+                    resultUnit.setText(destUnitSpinner.getSelectedItem().toString());
+
                 } else {
-                    resultsOutput.setText("0");
+
+                    resultsOutput.setText(String.format(Locale.ENGLISH,"%.2f", 0.0));
+                    resultUnit.setText(destUnitSpinner.getSelectedItem().toString());
+
                 }
             }
         });
     }
 
     static Double lengthConversion(String source, String dest, Double value) {
+        // Convert source to centimeters
         switch(source) {
             case "Inches":
                 value = value * 2.54;
@@ -69,8 +149,6 @@ public class MainActivity extends AppCompatActivity {
             case "Miles":
                 value = value * 160934;
                 break;
-            case "Centimeters":
-                break;
             case "Meters":
                 value = value * 100;
             case "Kilometers":
@@ -80,6 +158,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
+        // Convert centimeters to destination unit
         switch(dest) {
             case "Inches":
                 value = value / 2.54;
@@ -93,12 +172,84 @@ public class MainActivity extends AppCompatActivity {
             case "Miles":
                 value = value / 160934;
                 break;
-            case "Centimeters":
-                break;
             case "Meters":
                 value = value / 100;
             case "Kilometers":
                 value = value / 100000;
+                break;
+            default:
+                break;
+        }
+
+        return value;
+    }
+
+    static Double weightConversion(String source, String dest, Double value) {
+        // Convert to grams
+        switch(source) {
+            case "Ounce":
+                value = value * 28.3495;
+                break;
+            case "Pound":
+                value = value * 453.592;
+                break;
+            case "Ton":
+                value = value * 907185;
+                break;
+            case "Kilogram":
+                value = value * 1000;
+                break;
+            case "Tonne":
+                value = value * 1000000;
+                break;
+            default:
+                break;
+        }
+
+        // Convert grams to destination unit
+        switch(dest) {
+            case "Ounce":
+                value = value / 28.3495;
+                break;
+            case "Pound":
+                value = value / 453.592;
+                break;
+            case "Ton":
+                value = value / 907185;
+                break;
+            case "Kilogram":
+                value = value / 1000;
+                break;
+            case "Tonne":
+                value = value / 1000000;
+                break;
+            default:
+                break;
+        }
+
+        return value;
+    }
+
+    static Double tempConversion(String source, String dest, Double value) {
+        // Convert to Celcius
+        switch(source) {
+            case "Fahrenheit":
+                value = (value - 32) / 1.8;
+                break;
+            case "Kelvin":
+                value = value - 273.15;
+                break;
+            default:
+                break;
+        }
+
+        // Convert Celcius to destination unit
+        switch(dest) {
+            case "Fahrenheit":
+                value = (value * 1.8) + 32;
+                break;
+            case "Kelvin":
+                value = value + 273.15;
                 break;
             default:
                 break;
